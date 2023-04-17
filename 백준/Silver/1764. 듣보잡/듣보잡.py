dcbe6bb61370101c9e633aad7d32e1c6294fee4a
MinjoonHK <108560916+MinjoonHK@users.noreply.github.com>
2023-04-17 23:41:03 +0800

    [Silver IV] Title: 듣보잡, Time: 3732 ms, Memory: 39192 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+arr1 = dict()
+answer = []
+for i in range(n):
+    x = input()
+    if x not in arr1:
+        arr1[x] = i
+
+for i in range(m):
+    y = input()
+    if y in arr1:
+        answer.append(y)
+
+answer.sort()
+print(len(answer))
+print(*answer, sep='\n')
