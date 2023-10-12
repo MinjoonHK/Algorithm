a0a90274de2f362400c08b09f9afba039b77a85a
MinjoonHK <108560916+MinjoonHK@users.noreply.github.com>
2023-10-12 15:40:35 +0800

    [D1] Title: 1대1 가위바위보, Time: 142 ms, Memory: 58,264 KB -BaekjoonHub

--- SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.py
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.py
@@ -1,5 +1,16 @@
-A, B = map(int,input().split())
-if A > B:
-    print("A")
-else:
-    print("B")
+A,B = map(int,input().split())
+if A + B == 3:
+    if A > B:
+        print("A")
+    else:
+        print("B")
+elif A + B == 4:
+    if A > B:
+        print("B")
+    else:
+        print("A")
+elif A + B == 5:
+    if A > B:
+        print("A")
+    else:
+        print("B")
