cf3b18dffad897141173743f6b5292c7dbbb7a72
MinjoonHK <108560916+MinjoonHK@users.noreply.github.com>
2023-10-13 16:07:35 +0800

    [D2] Title: 가랏! RC카!, Time: 145 ms, Memory: 58,252 KB -BaekjoonHub

--- SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.py
+++ SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.py
@@ -1,21 +1,18 @@
 T = int(input())
 for i in range(1,T+1):
-    user_input = int(input())
+    N = int(input())
     result = 0
     speed = 0
-    for j in range(user_input):
-        user_command = input().split()
-        if int(user_command[0]) == 1:
-            speed += int(user_command[1])
+    for j in range(N):
+        arr = list(map(int,input().split()))
+        if arr[0] == 0:
             result += speed
-
-        elif int(user_command[0]) == 0:
+        elif arr[0] == 1:
+            speed += arr[1]
             result += speed
-
-        elif int(user_command[0]) == 2:
-            speed -= int(user_command[1])
+        elif arr[0] == 2:
+            speed -= arr[1]
             if speed < 0:
                 speed = 0
             result += speed
-
     print(f"#{i}",result)
