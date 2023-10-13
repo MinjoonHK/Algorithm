90822b408303c038d0261eb24b26d06162511225
MinjoonHK <108560916+MinjoonHK@users.noreply.github.com>
2023-10-13 10:36:42 +0800

    [D2] Title: 수도 요금 경쟁 D2 Attack, Time: 145 ms, Memory: 58,244 KB -BaekjoonHub

--- SWEA/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.py
+++ SWEA/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.py
@@ -1,9 +1,12 @@
 T = int(input())
 for i in range(1,T+1):
     P,Q,R,S,W = map(int,input().split())
-    company1 = P * W
+    A = P * W
     if R > W:
-        company2 = Q
-    elif R < W:
-        company2 = Q + (S * (W - R))
-    print(f"#{i}",min(company1,company2))
+        B = Q
+    else:
+        B = Q + ((W-R) * S)
+    if B > A:
+        print(f"#{i}",A)
+    elif B < A:
+        print(f"#{i}",B)
