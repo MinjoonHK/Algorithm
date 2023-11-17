54e22d2e84c0cf19c2b3ba63c25699e24db104d9
MinjoonHK <108560916+MinjoonHK@users.noreply.github.com>
2023-11-17 22:32:14 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(str + "??!");
+    }
+}
