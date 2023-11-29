6af1fd3a5dca85764ef592c6dd07989fd115c20a
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2023-11-29 15:13:06 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 640 ms, Memory: 67996 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -1,27 +1,31 @@
 import sys
-sys.setrecursionlimit(10**9)
+sys.setrecursionlimit(10 ** 6)
 input = sys.stdin.readline
 
-n, m, r = map(int, input().split())
-graph = [[] for _ in range(n + 1)]
-visited = [0] * (n + 1)
-count = 1
+N, M, R = map(int,input().split())
 
-for _ in range(m):
-    a, b = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+visited = [0] * (N+1)
+c = 1
+
+for _ in range(M):
+    a,b = map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
 
 def dfs(v):
-    global count
-    visited[v] = count
-    graph[v].sort()
-    for g in graph[v]:
-        if visited[g] == 0:
-            count += 1
-            dfs(g)
+    global c
+    visited[v] = c
+    for i in graph[v]:
+        if visited[i] == 0:
+            c += 1
+            dfs(i)
 
-dfs(r)
+for i in range(N+1):
+    graph[i].sort()
 
-for i in range(1, n + 1):
-    print(visited[i])
+dfs(R)
+
+for j in range(1, N+1):
+    print(visited[j])
+
