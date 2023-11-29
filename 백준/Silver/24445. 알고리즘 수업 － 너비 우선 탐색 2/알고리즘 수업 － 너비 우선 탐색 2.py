d567ecdbb31239a612b685b8e140333663ab7e5a
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2023-11-29 15:32:59 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 608 ms, Memory: 58620 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,38 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+N, M, R = map(int,input().split())
+
+graph = [[] for _ in range(N+1)]
+visited = [0] * (N+1)
+c = 1
+
+for _ in range(M):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def bfs(v):
+    global c
+    queue = deque()
+    queue.append(v)
+    visited[v] = c
+    while queue:
+        V = queue.popleft()
+        for i in graph[V]:
+            if visited[i] == 0:
+                c += 1
+                visited[i] = c
+                queue.append(i)
+
+for i in range(N+1):
+    graph[i].sort(reverse = True)
+
+bfs(R)
+
+for j in range(1, N+1):
+    print(visited[j])
+
+
