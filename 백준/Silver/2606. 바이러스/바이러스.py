aee18bcacf07c48d6b47f9da8247cf949d09bf6a
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2023-11-30 14:18:20 +0900

    [Silver III] Title: 바이러스, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,16 +1,21 @@
-T = int(input())
 N = int(input())
-graph = [[] for _ in range(T+1)]
-visited = [0] * (T+1)
-for _ in range(N):
-    a,b = map(int,input().split())
-    graph[a] += [b]
-    graph[b] += [a]
+M = int(input())
 
-def dfs(V):
-    visited[V] = 1
-    for i in graph[V]:
-        if visited[i] == 0:
+graph = [[] for _ in range(N+1)]
+visited = [False] * (N+1)
+for i in range(M):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+counter = 0
+
+def dfs(v):
+    global counter
+    visited[v] = True
+    for i in graph[v]:
+        if not visited[i]:
+            counter += 1
             dfs(i)
 dfs(1)
-print(sum(visited)-1)
+print(counter)
