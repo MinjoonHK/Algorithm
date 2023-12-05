643c6434018b6bf08afc82f350213e18dfdb879d
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2023-12-05 21:11:23 +0900

    [Bronze V] Title: 나머지, Time: 124 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,19 +1,15 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-public class Main {
+import java.io.*;
+import java.util.*;
+class Main {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        StringTokenizer st = new StringTokenizer(str, " ");
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
-
-        System.out.println((A+B)%C);
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st = new StringTokenizer(br.readLine());
+       int A = Integer.parseInt(st.nextToken());
+       int B = Integer.parseInt(st.nextToken());
+       int C = Integer.parseInt(st.nextToken());
+       System.out.println( (A+B)%C);
         System.out.println(((A%C) + (B%C))%C);
-        System.out.println((A*B)%C);
-        System.out.println(((A%C) * (B%C))%C);
+        System.out.println( (A*B)%C);
+        System.out.println( ((A%C) * (B%C))%C);
     }
 }
