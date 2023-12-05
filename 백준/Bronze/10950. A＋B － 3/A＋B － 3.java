c56f68be9a55346be758138cf5042dfe0af6b8d9
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2023-12-05 22:04:03 +0900

    [Bronze V] Title: A+B - 3, Time: 128 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -1,17 +1,17 @@
-    import java.io.*;
-    import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
-    public class Main {
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-            int N = Integer.parseInt(br.readLine());
-            for(int i = 0;i<N;i++){
-                String str = br.readLine();
-                StringTokenizer st = new StringTokenizer(str, " ");
-                int A = Integer.parseInt(st.nextToken());
-                int B = Integer.parseInt(st.nextToken());
-                System.out.println(A+B);
-            }
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for(int i =0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(a+b);
         }
     }
+}
