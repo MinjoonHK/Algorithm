87bc4898985e3dc35b46570b5a1755b3d6c41869
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2023-12-05 22:05:39 +0900

    [Bronze V] Title: 합, Time: 120 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,15 +1,16 @@
-    import java.io.*;
-    import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
-    public class Main {
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            int a = Integer.parseInt(br.readLine());
-            int answer = 0;
-            for(int i = 1; i <a+1; i++){
-                answer += i;
-            }
-            System.out.println(answer);
-
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int answer = 0;
+        for (int i =1; i<=N; i++){
+            answer += i;
         }
+        System.out.println(answer);
     }
+}
