27f8e8d228282826b5eef8d977af2aa240d3fa95
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2023-12-05 21:29:55 +0900

    [Bronze V] Title: 사분면 고르기, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -1,22 +1,21 @@
-    import java.io.*;
-    import java.util.*;
-
-    public class Main {
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            int A = Integer.parseInt(br.readLine());
-            int B = Integer.parseInt(br.readLine());
-            if(A > 0 && B > 0){
-                System.out.println(1);
-            }
-            else if(A < 0 && B > 0){
-                System.out.println(2);
-            }
-            else if(A < 0 && B < 0){
-                System.out.println(3);
-            }
-            else{
-                System.out.println(4);
-            }
+import java.io.*;
+import java.util.*;
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        if( a>0 && b >0){
+            System.out.println(1);
+        }
+        else if(a<0 && b >0){
+            System.out.println(2);
+        }
+        else if(a<0 && b <0){
+            System.out.println(3);
+        }
+        else {
+            System.out.println(4);
         }
     }
+}
