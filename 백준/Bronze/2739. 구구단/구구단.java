6205ff56c45d9bd461f6951dae9b1c7611539c32
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2023-12-05 22:00:04 +0900

    [Bronze V] Title: 구구단, Time: 156 ms, Memory: 16260 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,12 +1,14 @@
-    import java.io.*;
-    import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
-    public class Main {
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            int A = Integer.parseInt(br.readLine());
-            for(int i = 1;i<10;i++){
-                System.out.println(A+" * "+i+" = "+A*i);
-            }
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        for(int i =1; i <10;i++){
+            System.out.println(a + " * " + i + " = " + a*i);
         }
     }
+}
