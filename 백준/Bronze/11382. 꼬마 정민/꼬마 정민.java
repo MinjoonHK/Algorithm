4b8000e7e4c962b4f79e6d2d1754eebd71f291ec
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2023-12-05 21:18:26 +0900

    [Bronze V] Title: 꼬마 정민, Time: 124 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -1,13 +1,12 @@
 import java.io.*;
 import java.util.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        long A = Long.parseLong(st.nextToken());
-        long B = Long.parseLong(st.nextToken());
-        long C = Long.parseLong(st.nextToken());
-        System.out.print(A + B + C);
+class Main {
+    public static void main(String[] args) throws Exception {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st = new StringTokenizer(br.readLine());
+       long a = Long.parseLong(st.nextToken());
+       long b = Long.parseLong(st.nextToken());
+       long c = Long.parseLong(st.nextToken());
+       System.out.println(a+b+c);
     }
 }
