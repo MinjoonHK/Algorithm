e06be42fdeb24fc87352506d334643926a47b74a
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2023-12-05 22:44:48 +0900

    [Bronze V] Title: A+B - 5, Time: 132 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (a ==0 && b == 0){
+                break;
+            }
+            else{
+                System.out.println(a+b);
+            }
+        }
+    }
+}
