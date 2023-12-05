f63b5013bb79547d8684a4765558217b2aa09a5f
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2023-12-05 21:21:55 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/10172. 개/개.java
+++ 백준/Bronze/10172. 개/개.java
@@ -1,12 +1,12 @@
 import java.io.*;
 import java.util.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        System.out.println("|\\_/|");
-        System.out.println("|q p|   /}");
-        System.out.println("( 0 )\"\"\"\\");
-        System.out.println("|\"^\"`    |");
-        System.out.println("||_/=\\\\__|");
+class Main {
+    public static void main(String[] args) throws Exception {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       System.out.println("|\\_/|");
+       System.out.println("|q p|   /}");
+       System.out.println("( 0 )\"\"\"\\");
+       System.out.println("|\"^\"`    |");
+                System.out.println("||_/=\\\\__|");
     }
 }
