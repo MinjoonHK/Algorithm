28aa2d0955538d57b1f0eb973d9032f9fdee509f
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2023-12-05 21:14:34 +0900

    [Bronze III] Title: 곱셈, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -1,20 +1,13 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-public class Main {
+import java.io.*;
+import java.util.*;
+class Main {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-
-        int A = Integer.parseInt(br.readLine());
-        String B = br.readLine();
-
-        char[] b = B.toCharArray();
-        
-        System.out.println(A * (b[2]-'0'));
-        System.out.println(A * (b[1]-'0'));
-        System.out.println(A * (b[0]-'0'));
-        System.out.println(A * Integer.parseInt(B));
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       int a = Integer.parseInt(br.readLine());
+       int b = Integer.parseInt(br.readLine());
+       System.out.println(a*(b%10));
+       System.out.println(a*((b%100)/10));
+       System.out.println(a*(b/100));
+       System.out.println(a*b);
     }
 }
