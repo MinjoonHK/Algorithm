e87c28928cd916cda229ed1756e1bd60b03dced9
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2023-12-05 21:07:42 +0900

    [Bronze V] Title: ??!, Time: 124 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,11 +1,9 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-public class Main {
+import java.io.*;
+import java.util.*;
+class Main {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        System.out.println(str + "??!");
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       String s = br.readLine();
+       System.out.println(s + "??!");
     }
 }
