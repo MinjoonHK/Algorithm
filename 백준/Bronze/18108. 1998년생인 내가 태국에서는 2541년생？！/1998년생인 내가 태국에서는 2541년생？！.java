e4eefae3818abbc424f20ad7040178b396806f0a
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2023-12-05 21:08:51 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -1,12 +1,9 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-public class Main {
+import java.io.*;
+import java.util.*;
+class Main {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        int a = Integer.parseInt(str);
-        System.out.println(a - 543);
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       int a = Integer.parseInt(br.readLine());
+       System.out.println(a - 543);
     }
 }
