72a0873ddd3766dfadca6cff0d3d9c2936190e70
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2023-12-05 22:47:49 +0900

    [Bronze V] Title: A+B - 4, Time: 120 ms, Memory: 14044 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        String str;
+        while( (str=br.readLine()) != null ) {
+
+            st = new StringTokenizer(str, " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a + b).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
