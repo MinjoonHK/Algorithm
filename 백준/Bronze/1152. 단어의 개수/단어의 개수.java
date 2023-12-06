a2bdcaf50152595888cdb6d264d035c051f1634f
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2023-12-07 00:08:24 +0900

    [Bronze II] Title: 단어의 개수, Time: 272 ms, Memory: 20028 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        System.out.print(st.countTokens());
+    }
+}
