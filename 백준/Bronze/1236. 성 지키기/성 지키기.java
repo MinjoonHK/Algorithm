9917b6779e69ab9f8fa6cc0999dd8125ab670b00
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2023-12-07 20:40:07 +0900

    [Bronze I] Title: 성 지키기, Time: 224 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Bronze/1236. 성 지키기/성 지키기.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();	// 세로
+		int M = scan.nextInt();	// 가로
+		int row = 0;	// 행
+		int col = 0;	// 열
+		char[][] map = new char[N][M];
+		for(int i=0; i<map.length; i++) {
+			String str = scan.next();
+			for(int j=0; j<map[i].length; j++) {
+				map[i][j] = str.charAt(j);
+			}
+		}
+		
+		// 행에 필요한 경비원 수
+		for(int i=0; i<N; i++) {
+			boolean flag = true;
+			for(int j=0; j<M; j++) {
+				if(map[i][j] == 'X') {
+					flag = false;
+					break;
+				}
+			}
+			if(flag)
+				row ++;
+		}
+		
+		// 열에 필요한 경비원 수
+		for(int i=0; i<M; i++) {
+			boolean flag = true;
+			for(int j=0; j<N; j++) {
+				  if(map[j][i] == 'X') {
+					  flag = false;
+					  break;
+				  }
+			}
+			if(flag)
+				col ++;
+		}
+		
+		// 행, 열 중 더 많은 경비원 추가
+		System.out.println(Math.max(row, col));
+		scan.close();
+	}
+
+}
