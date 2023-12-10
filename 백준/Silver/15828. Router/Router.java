2fa285328341100b46b516cbe6318105429a1fa5
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2023-12-10 16:01:00 +0900

    [Silver IV] Title: Router, Time: 296 ms, Memory: 24052 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/15828. Router/Router.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int buffer = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+        while(true){
+            int cmd = Integer.parseInt(br.readLine());
+            if(cmd == -1){
+                break;
+            }
+            else if(cmd == 0){
+                queue.poll();
+            }
+            else{
+                queue.offer(cmd);
+            }
+        }
+        for(int i:queue){
+            sb.append(i);
+            sb.append(" ");
+        }
+        System.out.println(sb);
+    }
+}
