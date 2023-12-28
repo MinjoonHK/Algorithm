fd1c3d62c5d73bb0fb69297d52395b4315296aba
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2023-12-28 16:47:03 +0900

    [Bronze III] Title: 최댓값, Time: 152 ms, Memory: 16148 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,33 +1,28 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
 
-		int i,j;
-		int max = 0;
-		int x = 1, y = 1;
-		
-		int arr[][] = new int[9][9];
-		
-		for(i=0;i<9;i++) {
-			for(j=0;j<9;j++) {
-				arr[i][j] = sc.nextInt();
-			}
-		}
-		
-		for(i=0;i<9;i++) {
-			for(j=0;j<9;j++) {
-				if(arr[i][j]>max) {
-					max = arr[i][j];
-					x = i+1;
-					y = j+1;
-				}
-			}
-		}
-		sc.close();
-		
-		System.out.println(max);
-		System.out.println(x+" "+y);
-	}
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int[][] arr = new int[9][9];
+        int max = 0;
+        int row = 1;
+        int column = 1;
+
+        for(int i=0; i<9; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<9; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if(arr[i][j] > max){
+                    max = arr[i][j];
+                    row = i+1;
+                    column = j+1;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.println(row +" "+column);
+    }
 }
