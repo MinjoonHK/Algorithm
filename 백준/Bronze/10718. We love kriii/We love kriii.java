e7b2c3bc644c10a68940de6a409ab9d13429e9e1
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2024-01-16 11:05:24 +0900

    [Bronze V] Title: We love kriii, Time: 112 ms, Memory: 13900 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args)throws Exception{
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
