3495a3cc8adf1f59d19a5674c3e0d3c934e8c1f2
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2024-01-30 11:40:32 +0900

    [Bronze IV] Title: 베라의 패션, Time: 124 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        System.out.println((N-1) * N);
+        
+
+    }
+}
