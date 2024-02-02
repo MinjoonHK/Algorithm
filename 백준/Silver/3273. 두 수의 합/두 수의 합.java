5e3588c78e19064a3b40dcaa1a4139eaaa0f727a
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2024-02-03 00:09:50 +0900

    [Silver III] Title: 두 수의 합, Time: 324 ms, Memory: 25092 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,44 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String args[]) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.valueOf(st.nextToken());
-		st = new StringTokenizer(br.readLine());
-		int[] arr = new int[n];
-		for(int i = 0; i < n; i++) {
-			arr[i] = Integer.valueOf(st.nextToken());
-		}
-		
-		Arrays.parallelSort(arr);
-		
-		int x = Integer.valueOf(br.readLine());
-		int count = 0;
-		int start = 0;
-		int end = n - 1;
-		int sum = 0;
-		
-		while(start < end) {
-			sum = arr[start] + arr[end];
-			if(sum == x) {
-				count++;
-			}
-			
-			if(sum <= x) {
-				start++;
-			}
-			else {
-				end--;
-			}
-		}
-		
-		
-		System.out.println(count);
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int x = Integer.parseInt(br.readLine()); // ai + aj 가 되는 수
+
+        Arrays.sort(arr);
+
+        int start = 0;
+        int end = N-1;
+        int counter = 0;
+        int sum = 0;
+
+        while(start < end){
+            sum = arr[start] + arr[end];
+            if(sum == x){
+                counter++;
+            }
+            if(sum <= x){ // 만약 합이 작다면 스타트 하나 옮기기
+                start++;
+            }
+            else{ //만약 합이 크다면 엔드 하나 줄이기
+                end--;
+            }
+        }
+        System.out.println(counter);
+    }
 }
+
