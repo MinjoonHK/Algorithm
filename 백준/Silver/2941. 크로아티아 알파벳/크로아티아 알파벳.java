bf08e7b5dd8e6af80077bce169ff7cab36085412
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2024-02-09 21:32:28 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+    
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		
+		int len = str.length();
+		int count = 0;
+ 
+		for (int i = 0; i < len; i++) {
+ 
+			char ch = str.charAt(i);
+ 
+			if(ch == 'c' && i < len - 1) {
+				
+				if(str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-') {		
+					
+					i++;		
+				}
+				
+			}
+		    
+			else if(ch == 'd' && i < len - 1) {
+				if(str.charAt(i + 1) == '-') {
+						i++;
+					}
+				else if(str.charAt(i + 1) == 'z' && i < len - 2) {
+					
+					if(str.charAt(i + 2) == '=') {
+						i += 2;
+					}
+				}
+			}
+		    
+			else if((ch == 'l' || ch == 'n') && i < len - 1) {
+				if(str.charAt(i + 1) == 'j') {
+					i++;
+				}
+			}
+		    
+ 
+			else if((ch == 's' || ch == 'z') && i < len - 1) {
+				if(str.charAt(i + 1) == '=') {
+					i++;
+				}
+			
+		    }
+		    
+			count++;
+ 
+		}
+ 
+		System.out.println(count);
+	}
+}
