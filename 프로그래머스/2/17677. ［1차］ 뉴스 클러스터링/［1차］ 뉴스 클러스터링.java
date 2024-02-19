ef1d1776a430fb5ab13b8cbfb564e2f3bba0ace2
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2024-02-19 13:45:56 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 2.05 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -0,0 +1,64 @@
+import java.util.*;
+import java.io.*;
+
+
+class Solution {
+    public int solution(String str1, String str2) {
+       str1 = str1.toLowerCase();
+		str2 = str2.toLowerCase();
+		
+		ArrayList<String> list1 = new ArrayList<>();
+		ArrayList<String> list2 = new ArrayList<>();
+		ArrayList<String> union = new ArrayList<>();
+		ArrayList<String> intersection = new ArrayList<>();
+
+		for(int i = 0; i < str1.length()-1; i++) {
+			char a = str1.charAt(i);
+			char b = str1.charAt(i+1);
+
+			if(Character.isLetter(a) && Character.isLetter(b)) {
+				String str = Character.toString(a) + Character.toString(b);
+				list1.add(str);
+			}
+		}
+
+		for(int i = 0; i < str2.length()-1; i++) {
+			char a = str2.charAt(i);
+			char b = str2.charAt(i+1);
+
+			if(Character.isLetter(a) && Character.isLetter(b)) {
+				list2.add(Character.toString(a) + Character.toString(b));
+			}
+        }
+
+
+		Collections.sort(list1);
+		Collections.sort(list2);
+		
+
+		for(String s : list1) {
+			if(list2.remove(s)) {
+				intersection.add(s);
+			}
+			union.add(s);
+		}
+		
+		
+		for(String s : list2) { 
+			union.add(s);
+		}
+        
+ 
+		double a = intersection.size();
+		double b = union.size();
+
+   		double jakard = 0;
+	
+		if(union.size() == 0)
+			jakard = 1;
+		else
+			jakard = (double) intersection.size() / (double) union.size();
+
+		return (int) (jakard * 65536);
+    }
+}
