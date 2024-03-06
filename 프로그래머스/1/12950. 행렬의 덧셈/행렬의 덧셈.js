79a6e1d6bba87e07a6e628c979851a0cb0d3d3c3
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2024-03-06 16:39:30 +0900

    [level 1] Title: 행렬의 덧셈, Time: 12.97 ms, Memory: 67.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -0,0 +1,11 @@
+function solution(arr1, arr2) {
+    var answer = [];
+    for(let i=0; i<arr1.length;i++){
+        let temp = [];
+        for(let j=0; j<arr1[i].length; j++){
+            temp.push(arr1[i][j] + arr2[i][j]);
+        }
+        answer.push(temp);
+    }
+    return answer;
+}
