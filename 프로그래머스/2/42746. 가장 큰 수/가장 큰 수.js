d01cfaddc89d51e74c2d28b4cb6e63be99e50343
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2024-03-20 22:44:23 +0900

    [level 2] Title: 가장 큰 수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -0,0 +1,9 @@
+function solution(numbers){
+    const answer = numbers
+	.map(numbers => String(numbers))
+    .sort((a,b) => (b+a) - (a+b))
+    .join('');
+    
+    return answer[0] === '0' ? '0' : answer;
+}
+    
