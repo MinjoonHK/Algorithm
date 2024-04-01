448cdcad2891630a24d21c8fae4c9b2ca0ae2da7
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2024-04-01 17:37:09 +0900

    [level 3] Title: 입국심사, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.js
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -1,16 +1,17 @@
 function solution(n, times) {
     var answer = 0;
-
     return s(n,times);
 }
 function s(n, times ) {
-    var min =0 ;
-    var max = n * Math.max(...times);
+    times.sort((a,b) => a-b); // 이분탐색은 정렬된 상태에서만 가능
+    var min =0 ; // 가장 시간이 적게 걸리는 경우
+    var max = n * Math.max(...times); //가장 시간이 오래걸리는 경우
     while (min <= max) {
         //중앙값
         var mid = Math.floor((min + max) / 2);
         //합구하기
         var maxInMid = times.reduce((acc,cur)=>acc += Math.floor(mid/cur) , 0);
+        
         if( n <= maxInMid) {
             max = mid -1;
         } else {
