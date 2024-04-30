435a2d682a891b9e62dcd54d7f514f30f521ab21
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2024-04-30 22:22:54 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 30.12 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,34 +1,31 @@
 function solution(maps) {
     let N = maps.length;
     let M = maps[0].length;
-    
-    let visited = new Array(N).fill().map((v)=> new Array(M).fill(0));
+    let visited = Array.from(Array(N), () => Array(M).fill(0));
     let dx = [-1,1,0,0];
     let dy = [0,0,-1,1];
-    
-    const bfs = (x,y) =>{
+    function bfs(x,y){
         let q = [];
         q.push([x,y]);
-        visited[x][y] = 1;
-        
-        while(!q.length == 0){
-            let [curX,curY] = q.shift();
-            
+        while(q.length !=0){
+            let qPoll = q.shift();
+            let curX = qPoll[0];
+            let curY = qPoll[1];
             for(let i=0; i<4; i++){
-                let nx = curX+dx[i];
-                let ny = curY+dy[i];
-                if(nx <0 || ny < 0 || nx >= N || ny >= M){
+                let nx = curX + dx[i];
+                let ny = curY + dy[i];
+                
+                if(nx >= N || ny >= M || nx < 0 || ny < 0){
                     continue;
                 }
-                if(maps[nx][ny] == 1 && !visited[nx][ny]){
-                    visited[nx][ny] = visited[curX][curY] + 1;
+                if(!visited[nx][ny] && maps[nx][ny] ==1){
+                    maps[nx][ny] = maps[curX][curY] + 1;
+                    visited[nx][ny] = true;
                     q.push([nx,ny]);
                 }
             }
         }
     }
-    
     bfs(0,0);
-    return visited[N-1][M-1] ? visited[N-1][M-1] : -1;
-
+    return maps[N-1][M-1] <= 1 ? -1 : maps[N-1][M-1];
 }
