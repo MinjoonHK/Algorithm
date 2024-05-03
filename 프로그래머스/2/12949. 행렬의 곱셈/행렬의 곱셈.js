4ba944c79542dcafaccf3eaf298d98868844d948
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2024-05-04 02:29:26 +0900

    [level 2] Title: 행렬의 곱셈, Time: 3.17 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -4,13 +4,15 @@ function solution(matrix1, matrix2) {
 
     // 결과 행렬의 크기 결정
     for (var i = 0; i < matrix1.length; i++) {
-        result[i] = [];
+        let temp=[];
         for (var j = 0; j < matrix2[0].length; j++) {
-            result[i][j] = 0;
+            let temp2 = 0;
             for (var k = 0; k < matrix1[0].length; k++) {
-                result[i][j] += matrix1[i][k] * matrix2[k][j];
+                temp2 += matrix1[i][k] * matrix2[k][j];
             }
+            temp.push(temp2);
         }
+        result.push(temp);
     }
     return result;
 }
