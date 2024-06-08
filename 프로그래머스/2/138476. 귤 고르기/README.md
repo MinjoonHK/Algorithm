d6f154304d202d97c87f1ed6047b2aee56688b34
Minjoon Park <108560916+MinjoonHK@users.noreply.github.com>
2024-06-08 10:32:59 +0900

    [level 2] Title: 귤 고르기, Time: 35.58 ms, Memory: 51.6 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/README.md
+++ 프로그래머스/2/138476. 귤 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476?language=javascript) 
 
 ### 성능 요약
 
-메모리: 52.1 MB, 시간: 39.16 ms
+메모리: 51.6 MB, 시간: 35.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 27일 11:00:21
+2024년 06월 08일 10:32:56
 
 ### 문제 설명
 
--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,26 +1,25 @@
 function solution(k, tangerine) {
-    let dict = new Map();
-    
+    let answer = 0;
+    let map = new Map();
     for(let i=0; i<tangerine.length; i++){
-        if(dict.has(tangerine[i])){
-            dict.set(tangerine[i],dict.get(tangerine[i])+1);
+        if(map.get(tangerine[i]) == undefined){
+            map.set(tangerine[i],1);
         }
         else{
-            dict.set(tangerine[i],1);
+            map.set(tangerine[i],map.get(tangerine[i]) +1);
         }
     }
-    
-    let arr = [...dict];
-    arr.sort((a,b) => b[1]-a[1]);
-    let answer = 0;
-    for(let i=0; i<arr.length; i++){
-        
-        answer += 1;
-        k -= parseInt(arr[i][1]);
-        if(k <= 0){
+    let mapToArray = Array.from(map);
+    mapToArray.sort((a,b) => b[1] - a[1]);
+    for(let i=0; i<mapToArray.length; i++){
+        if(mapToArray[i][1] >= k){
+            answer += 1;
             break;
         }
+        else{
+            k -= mapToArray[i][1];
+            answer += 1;
+        }
     }
-    
     return answer;
 }
