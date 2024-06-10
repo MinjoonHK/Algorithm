087f045d3b2e8973c9793ae0d9f3f096e6468153
Minjoon Park 박민준 <minjoon.park.hk@gmail.com>
2024-06-10 13:12:42 +0900

    [level 3] Title: 야근 지수, Time: 21.84 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.js
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.js
@@ -1,20 +1,19 @@
 function solution(n, works) {
-  if(works.reduce((a,b) => a+b) <= n) return 0;
-  
-  let sorted = works.sort((a,b) => a-b);
-  const len = works.length;
-  
-  while(n) {
-    const max = sorted[len-1];
-    
-    for(let i = len-1; i >= 0; i--) {
-      if(sorted[i] >= max) {
-        sorted[i]--;
-        n--;
-      }
-      if(!n) break;
+    if(works.reduce((a,b) => a+b) <= n) return 0;
+    works.sort((a,b) => b-a);
+    while(n){
+        
+        let max = works[0];
+        for(let i=0; i<works.length; i++){
+            if(works[i] >= max){
+                works[i]--;
+                n--;
+            }
+            if(!n){
+            break;
+        }
+        }
+
     }
-  }
-  
-  return sorted.reduce((a,b) => a + Math.pow(b, 2), 0);
+    return works.reduce((acc,cur) => acc + Math.pow(cur,2),0);
 }
