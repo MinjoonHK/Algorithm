390c65107dd1ba576ce305600e533a74ef4adf94
Minjoon Park 박민준 <minjoon.park.hk@gmail.com>
2024-09-21 16:22:31 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 168 ms, Memory: 14980 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,15 +1,15 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        for(int i=1;i<=N; i++) {
-            sb.append("*");
-            System.out.println(sb);
+        for(int i =0; i<N; i++){
+            for(int j=0; j < i+1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
-    }
+    }   
 }
