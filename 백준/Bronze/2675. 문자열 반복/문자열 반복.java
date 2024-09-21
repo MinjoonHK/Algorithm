8fd8b399aa11bdb252343ef1c62351875b71e2c7
Minjoon Park 박민준 <minjoon.park.hk@gmail.com>
2024-09-21 22:34:24 +0900

    [Bronze II] Title: 문자열 반복, Time: 132 ms, Memory: 16220 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,25 +1,25 @@
-import java.io.*;
+
+
 import java.util.*;
-import java.util.stream.IntStream;
+import java.io.*;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException{
-
+    public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-        for(int i=0; i<N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            String b = st.nextToken();
-            String[] arr = b.split("");
-            for(String c : arr){
-                bw.write(c.repeat(a));
-            }
-            bw.write("\n");
-        }
-        bw.flush();
 
+        for(int i=0; i<N; i++){
+            String answer ="";
+            String[] input = br.readLine().split(" ");
+            int R = Integer.parseInt(input[0]);
+            String S = input[1];
+            for(int j=0; j<S.length(); j++){
+                for(int k=0; k<R; k++){
+                    answer += S.charAt(j);
+                }
+            }
+            System.out.println(answer);
+        }
+        
     }
 }
