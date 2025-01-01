2eae940d6697c371a9c4223b748841322f7a4236
Minjoon Park 박민준 <minjoon.park.hk@gmail.com>
2025-01-01 15:15:28 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+N = int(input())
+if(N >= 90):
+    print("A")
+elif(N <90 and N >= 80):
+    print("B")
+elif(N <80 and N >= 70):
+    print("C")
+elif(N <70 and N >= 60):
+    print("D")
+else:
+    print("F")
