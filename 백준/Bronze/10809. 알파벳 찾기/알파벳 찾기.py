71ca83b80b83d33e73de82b93572aba92dc579f3
Minjoon Park 박민준 <minjoon.park.hk@gmail.com>
2025-01-01 21:17:58 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,7 @@
+alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+userInput = input()
+result = []
+for alphabet in alphabets:
+    result.append(userInput.find(alphabet))
+
+print(" ".join(map(str, result)))
